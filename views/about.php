e964f95d079de9e8a45a54f1a06ff1b6994636a2
Jeraldo0803 <108305413+Jeraldo0803@users.noreply.github.com>
2024-08-27 09:59:15 +0800

    About us page

--- views/about.php
+++ views/about.php
@@ -6,7 +6,40 @@
 
 <body>
 
-    <h1>About page</h1>
+    <div class="row no-gutters">
+        <div class="col">
+            <div class="card">
+                <img class="card-img img-fluid" style="object-fit: cover; box-shadow: inset 0 0 0 1000px rgba(0,0,0,.5  );" 
+                src="../assets/img/2nd-page.jpg" alt="">
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div class="container text-center text-white">
+                        <h2>謙虛<br>Loyalty</h2>
+
+
+                        <br><h2>忠誠<br>Humility</h2>
+
+                        
+                        <br><h2>仁愛<br>Benevolence</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img class="card-img img-fluid" src="../assets/img/2nd-page-2.jpg" alt="">
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div class="container bg-black text-center text-white">
+                        <h2>Mission</h2>
+                        <br>
+                        <p><bold class="fw-bolder">LORENZO RUIZ ACADEMY</bold> is a highly recognized Filipino-Chinese Dominican School 
+                            deeply dedicated to the wholesome formation of every student through formal and 
+                            innovative education that fosters a higher sense of responsibility, discipline, humility, loyalty, 
+                            and excellence among the members of its community, as inspired by its patron - Saint Lorenzo Ruiz.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php include $_SERVER['DOCUMENT_ROOT'] . '\views\templates\footer.php'; ?>
 
--- views/home.php
+++ views/home.php
@@ -6,14 +6,17 @@
 <body>
 
     <div class="card">
-        <div class="container">
-            <h1>Welcome to Lorenzo Ruiz Academy!</h1>
-            <h4>Education at the Heart of Chinatown</h4>
-            <h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio accusamus quasi
+        <img class="card-img img-fluid" style="object-fit: cover; height:500px;" src="../assets/img/front-page-bg.jpg" alt="Front Page Image">
+        <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div class="container text-white mt-5">
+            <h2 class="fw-bolder">Welcome to Lorenzo Ruiz Academy!</h2>
+            <h5 class="fw-bold">Education at the Heart of Chinatown</h5>
+            <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio accusamus quasi
                 <br>nisi deserunt dolore minima omnis voluptas quibusdam repudiandae veniam tempora
                 <br>perferendis est numquam eos, explicabo fugit vero doloremque fugiat?
-            </h5>
+            </h6>
             <a class="btn custom-btn text-black" href="#">Apply Now</a>
+        </div>
         </div>
     </div>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '\views\templates\footer.php'; ?>
